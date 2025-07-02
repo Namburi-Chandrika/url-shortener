d53eb7bd3e4346869f4889a24240526e3e1f4a54
Namburi Chandrika <140413100+Namburi-Chandrika@users.noreply.github.com>
2025-07-02 16:46:00 +0530

    Add files via upload

---
+++ url_shortener_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pyshorteners as pyst
+import pyperclip
+
+shortener=pyst.Shortener()
+def copying():
+  pyperclip.copy(shortener_url)
+st.markdown("<h1 style='text-align:center;'>URL SHORTENER</h1>",unsafe_allow_html=True)
+form=st.form("Name")
+url=form.text_input("Place URL HERE")
+s_btn=form.form_submit_button("search")
+if s_btn:
+  shortener_url=shortener.tinyurl.short(url)
+  st.markdown("### Shortened URL")
+  st.markdown(f"<h6 style='text-align:center;'>{shortener_url}</h6>",unsafe_allow_html=True)
+  st.button("copy",on_click=copying)
+  
